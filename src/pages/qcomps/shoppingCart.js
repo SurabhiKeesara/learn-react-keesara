d33f586cd12ea6d7d4c9caa5917fdb368560ca2e
SurabhiKeesara <surabhi.keesara@gmail>
2024-02-14 15:37:46 -0500

    Replace with arrow function

--- src/pages/qcomps/shoppingCart.js
+++ src/pages/qcomps/shoppingCart.js
@@ -5,9 +5,11 @@ export default function RequestTracker() {
   const [completed, setCompleted] = useState(0);
 
   async function handleClick() {
-    setPending(pending + 1);
+    // with arrow functions, these are put in a queue
+    // put in a queue instead of using hook
+    setPending(p => p + 1);
     await delay(3000);
-    setPending(pending);
+    setPending(p => p - 1);
     setCompleted(completed + 1);
   }
 
